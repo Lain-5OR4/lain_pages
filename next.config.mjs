0be9957edf16009ede7f446a466043eef2f9642d
Lain-5OR4 <inazumahp@gmail.com>
2025-05-28 13:41:33 +0900

    [fix]: githubpages root path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
     images: {
         unoptimized: true,
     },
     output: "export",
+    trailingSlash: true,
+    distDir: "out",
+    ...(isProd && isGitHubPages ? {
+        basePath: "/lain_pages",
+        assetPrefix: "/lain_pages",
+    } : {}),
 };
 
 export default nextConfig;
